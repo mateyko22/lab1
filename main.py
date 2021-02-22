811cd5152a64799b08881ab4ab787111d27c427c
mateyko22 <petmat@o2.pl>
2021-02-22 12:00:34 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+from math import *
+
+# Zad1
+# zmienna1 = 2
+# zmienna2 = 65
+# zmienna3 = "Ala ma kota"
+# zmienna4 = "imie i nazwisko"
+# zmienna5 = 2.5
+# zmienna6 = 100.99
+# print(zmienna1)
+# print(zmienna2)
+# print(zmienna3)
+# print(zmienna4)
+# print(zmienna5)
+# print(zmienna6)
+
+# #Zad2
+# dodawanie = 3 + 4
+# odejmowanie = 10 - 8
+# mnożenie = 2 * 14
+# dzielenie = 15 / 3
+# potegowanie = 2 ** 4
+# dzielenie_calkowite = 20 // 6
+# print(dodawanie)
+# print(odejmowanie)
+# print(mnożenie)
+# print(dzielenie)
+# print(potegowanie)
+# print(dzielenie_calkowite)
+
+# #Zad3
+# a = 2
+# a+=2
+# print(a)
+# a-=1
+# print(a)
+# a*=3
+# print(a)
+# a/=4
+# print(a)
+# a **= 3
+# print(a)
+# a %= 2
+# print(a)
+
+#Zad4
+# print(exp(10))
+
+# #Zad5
+# imie = "MATEUSZ"
+# nazwisko = "PETKIEWICZ"
+# print(str.capitalize(imie) + " " + str.capitalize(nazwisko))
+
+# #Zad6
+# piosenka = "sia ra la la la pa ra pa la pa pa la"
+# print(str.count(piosenka, "la"))
+
+#Zad 7
+
